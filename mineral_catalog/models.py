11d8b32ea91a4f85da8fc0251081856738bc5e71
Kris Spencer <antisocialtrees@gmail.com>
2018-12-22 02:41:55 -0800

    Added slug field

--- mineral_catalog/models.py
+++ mineral_catalog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.utils.text import slugify
 
 MAX_LENGTH = 100
 
 
 class Mineral(models.Model):
     name = models.CharField(max_length=MAX_LENGTH)
+    slug = models.SlugField(unique=True, max_length=MAX_LENGTH)
     image_filename = models.CharField(max_length=MAX_LENGTH)
     image_caption = models.CharField(blank=True, max_length=MAX_LENGTH)
     category = models.CharField(blank=True, max_length=MAX_LENGTH)
@@ -26,7 +28,23 @@ class Mineral(models.Model):
     group = models.CharField(blank=True, max_length=MAX_LENGTH)
 
     class Meta:
-        ordering = ['name']
+        ordering = ['slug']
 
     def __str__(self):
         return self.name
+
+    def _get_unique_slug(self):
+        slug = slugify(self.name)
+        unique_slug = slug
+        num = 1
+        while Mineral.objects.filter(slug=unique_slug).exists():
+            unique_slug = '{}-{}'.format(slug, num)
+            num += 1
+
+        return unique_slug
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = self._get_unique_slug()
+
+        super().save(*args, **kwargs)
