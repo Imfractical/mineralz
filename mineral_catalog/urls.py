ab714028038481acaad10ff5d1e5b6ba6852fc24
Kris Spencer <antisocialtrees@gmail.com>
2018-12-22 02:42:15 -0800

    Add list view

--- mineral_catalog/urls.py
+++ mineral_catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 
+from . import views
+
 app_name = 'catalog'
 urlpatterns = [
-    
+    path('', views.list_minerals, name='list'),
 ]
--- mineral_catalog/views.py
+++ mineral_catalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Mineral
+
+
+def list_minerals(request, letter=None):
+    if letter:
+        minerals = Mineral.objects.filter(name__istartswith=letter)
+    else:
+        minerals = Mineral.objects.all()
+
+    return render(request, 'mineral_catalog/list.html', {'minerals': minerals})
