ec86b8f182cee3a9696f96a582a419a2ab34c67f
Kris Spencer <antisocialtrees@gmail.com>
2018-12-23 03:14:45 -0800

    Add field names to iterate thru

--- mineral_catalog/views.py
+++ mineral_catalog/views.py
@@ -18,5 +18,9 @@ def list_minerals(request, letter=None):
 
 def detail_mineral(request, mineral_slug):
     mineral = Mineral.objects.get(slug=mineral_slug)
+    fields = ('category', 'formula', 'strunz_classification', 'crystal_system',
+              'unit_cell', 'color', 'crystal_symmetry', 'cleavage',
+              'mohs_scale_hardness', 'luster', 'streak', 'diaphaneity', 'optical_properties',
+              'refractive_index', 'crystal_habit', 'specific_gravity', 'group')
 
-    return render(request, 'mineral_catalog/detail.html', {'mineral': mineral})
+    return render(request, 'mineral_catalog/detail.html', {'mineral': mineral, 'fields': fields})
