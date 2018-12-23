7ec983de1e3567fc6183fd0392558cbd680e264f
Kris Spencer <antisocialtrees@gmail.com>
2018-12-23 03:14:10 -0800

    Enable list view to be used with or without letter filtering, also add mineral detail view

--- mineral_catalog/urls.py
+++ mineral_catalog/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 app_name = 'catalog'
 urlpatterns = [
-    path('', views.list_minerals, name='list'),
+    path('', views.redirect_home),
+    path('list/', views.list_minerals, name='list'),
+    re_path(r'^list/(?P<filter>[a-zA-Z])/$', views.list_minerals, name='list_by'),
+    path('view/<slug:mineral_slug>/', views.detail_mineral, name='detail'),
 ]
